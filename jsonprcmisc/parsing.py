f35bff48c44f970eb5e8a3f559422ce5ac436413
P. S. Clarke <jsonrpcmisc@pscl4rke.net>
2025-06-26 18:02:54 +0100

    Handle incoming results

--- jsonprcmisc/parsing.py
+++ jsonprcmisc/parsing.py
@@ -14,19 +14,20 @@ from . import modelling
 Parsed = modelling.Message | modelling.IncomingError
 
 
-def parse_message(message: str | bytes) -> Parsed | List[Parsed]:
+def parse_incoming(message: str | bytes) -> Parsed | List[Parsed]:
     try:
         doc = json.loads(message)
     except json.JSONDecodeError as exc:
         LOG.debug("JSON error: %r", exc)
         return modelling.IncomingError(erroring.ERROR_PARSE)
     if isinstance(doc, list):  # a batch
-        return [decode_incoming(x) for x in doc]
-    return decode_incoming(doc)
+        return [decode_incoming_message(x) for x in doc]
+    return decode_incoming_message(doc)
 
 
-def decode_incoming(doc: dict) -> Parsed:
+def decode_incoming_message(doc: dict) -> Parsed:
     # Be very forgiving if receiving errors from the other side:
+    #   (we want to avoid a loop of firing an error back-and-to)
     if "error" in doc:
         return modelling.ErrorMessage(
             error=erroring.JsonRpcError(
@@ -36,21 +37,24 @@ def decode_incoming(doc: dict) -> Parsed:
             ),
             id=doc.get("id", None),
         )
-    id = None
     try:
         assert doc["jsonrpc"] == "2.0"
-        if "id" in doc:
-            id = doc["id"]
-            return modelling.QueryMessage(
-                method=doc["method"],
-                params=doc.get("params", None),
-                id=id,
-            )
-        else:
+        if "id" not in doc:
             return modelling.NotificationMessage(
                 method=doc["method"],
                 params=doc.get("params", None),
             )
+        elif "method" in doc:
+            return modelling.QueryMessage(
+                method=doc["method"],
+                params=doc.get("params", None),
+                id=doc["id"],
+            )
+        else:
+            return modelling.ResultMessage(
+                result=doc["result"],
+                id=doc["id"],
+            )
     except Exception as exc:
         LOG.debug("Invalid: %r", exc)
-        return modelling.IncomingError(erroring.ERROR_INVALID, id=id)
+        return modelling.IncomingError(erroring.ERROR_INVALID, id=doc.get("id"))
