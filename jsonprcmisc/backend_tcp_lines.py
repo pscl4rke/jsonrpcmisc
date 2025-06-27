1dbaef279c6fbb7f4a4843b9704793c9dd0a16c1
P. S. Clarke <jsonrpcmisc@pscl4rke.net>
2025-06-27 19:41:26 +0100

    Use build_agent

--- jsonprcmisc/backend_tcp_lines.py
+++ jsonprcmisc/backend_tcp_lines.py
@@ -1,5 +1,10 @@
 
+
 from contextlib import asynccontextmanager
+
+import asyncio
+
+from jsonprcmisc.agent import Agent
 
 
 @asynccontextmanager
@@ -15,3 +20,17 @@ async def consume(agent, reader):
     async for line in reader:
         print("Received %r" % line)
         await agent.inject_received_message(line)
+
+
+@asynccontextmanager
+async def build_agent(dispatcher, reader, writer):
+    async with asyncio.TaskGroup() as tg:
+
+        async def send(string):
+            text = string.encode("ascii") + b"\n"
+            print("Sending %r" % text)
+            writer.write(text)
+
+        agent = Agent(tg, send, dispatcher)
+        async with cancelling(tg.create_task(consume(agent, reader))):
+            yield agent
